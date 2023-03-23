d8515520fa9f9045a2f2601a050b3e8054155a7d
Kaan Cihan <88425393+KaanCihan@users.noreply.github.com>
2023-03-23 21:12:06 +0000

    Fri, Mar 24, 2023, 12:12 AM +03:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -7,23 +7,26 @@ int main(void)
     int height;
     do
     {
-    height = get_int("Height: ");
+        height = get_int("Height: ");
     }
-    while(height < 1 || height > 8);
+    while (height < 1 || height > 8);
 
     // TODO: Creating the pyramid
     for (int i = 0; i <= height; i++)
     {
+
         for (int j = 0; j <= height - i ; j++)
         {
             printf(" ");
         }
 
+        // TODO: Creating the blocks
         for (int x = 0; x < i; x++)
         {
             printf("#");
         }
 
+        // TODO: Space between blocks
         printf("  ");
 
         for (int x = 0; x < i; x++)
