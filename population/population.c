17a3ca0867c503dc77b8ee72e0ecdcaa7c760a89
Kaan Cihan <88425393+KaanCihan@users.noreply.github.com>
2023-03-23 20:11:37 +0000

    Thu, Mar 23, 2023, 8:11 PM +00:00

--- population/population.c
+++ population/population.c
@@ -4,9 +4,9 @@
 int main(void)
 {
     // TODO: Prompt for start size
-
+    int startingPop = get_int("What is the starting population?");
     // TODO: Prompt for end size
-
+    int endingPop = get_int("What is the ending population?")
     // TODO: Calculate number of years until we reach threshold
 
     // TODO: Print number of years
