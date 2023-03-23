8a28ccd5b4391a5e90cb49e40afa106f860c8fed
Kaan Cihan <88425393+KaanCihan@users.noreply.github.com>
2023-03-23 20:24:44 +0000

    Thu, Mar 23, 2023, 8:24 PM +00:00

--- population/population.c
+++ population/population.c
@@ -4,11 +4,10 @@
 int main(void)
 {
     // TODO: Prompt for start size
-    while
     int startingPop;
     do
     {
-       startingpop = get_int("What is the starting population?");
+       startingPop = get_int("Start size: ");
     }
     while (startingPop < 9);
 
@@ -16,18 +15,18 @@ int main(void)
     int endingPop;
     do
     {
-        endingPop = get_int("What is the ending population?");
+        endingPop = get_int("End size: ");
     }
     while (endingPop < startingPop);
 
     // TODO: Calculate number of years until we reach threshold
     int clonePop = startingPop;
-    int years;
+    int years = 0;
     while (clonePop < endingPop)
     {
-        clonePop = clonePop + clonePop/3 - clonePop/4
+        clonePop = clonePop + clonePop/3 - clonePop/4;
         years++;
     }
     // TODO: Print number of years
-    printf("Years: %i", years)
+    printf("Years: %i\n", years);
 }
